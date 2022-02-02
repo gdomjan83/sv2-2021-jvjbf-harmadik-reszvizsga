30885c96a3f7e6d36dc1dfca550079f2024900ed
J_C <gomjan83@gmail.com>
2022-02-02 20:51:39 +0100

    Vizsga utáni gyakorlás

--- src/main/java/movietheatres/MovieTheatreService.java
+++ src/main/java/movietheatres/MovieTheatreService.java
@@ -27,15 +27,21 @@ public class MovieTheatreService {
     }
 
     public List<String> findMovie(String title) {
-        Set<String> result = new LinkedHashSet<>();
-        for (Map.Entry<String, List<Movie>> actual : shows.entrySet()) {
-            for (Movie movie : actual.getValue()) {
-                if (title.equals(movie.getTitle())) {
-                    result.add(actual.getKey());
-                }
-            }
-        }
-        return new ArrayList<>(result);
+        return shows.entrySet().stream()
+                .filter(e -> e.getValue().stream()
+                        .anyMatch(m -> title.equals(m.getTitle())))
+                .map(m -> m.getKey())
+                .toList();
+
+//        Set<String> result = new LinkedHashSet<>();
+//        for (Map.Entry<String, List<Movie>> actual : shows.entrySet()) {
+//            for (Movie movie : actual.getValue()) {
+//                if (title.equals(movie.getTitle())) {
+//                    result.add(actual.getKey());
+//                }
+//            }
+//        }
+//        return new ArrayList<>(result);
     }
 
     private void fillMap(String line) {
@@ -58,18 +64,26 @@ public class MovieTheatreService {
     }
 
     public LocalTime findLatestShow(String title) {
-        List<Movie> movies = new ArrayList<>();
-        for (List<Movie> films : shows.values()) {
-            for (Movie actual : films) {
-                movies.add(actual);
-            }
-        }
-        return movies.stream()
+        return shows.entrySet().stream()
+                .flatMap(s -> s.getValue().stream())
                 .filter(m -> title.equals(m.getTitle()))
-                .sorted(new ScreeningTimeComparing().reversed())
-                .map(m -> m.getStartTime())
+                .sorted(Comparator.comparing(Movie::getStartTime).reversed())
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("No screening available."));
+                .orElseThrow(() -> new IllegalArgumentException("No screenign available.")).getStartTime();
+
+
+//        List<Movie> movies = new ArrayList<>();
+//        for (List<Movie> films : shows.values()) {
+//            for (Movie actual : films) {
+//                movies.add(actual);
+//            }
+//        }
+//        return movies.stream()
+//                .filter(m -> title.equals(m.getTitle()))
+//                .sorted(new ScreeningTimeComparing().reversed())
+//                .map(m -> m.getStartTime())
+//                .findFirst()
+//                .orElseThrow(() -> new IllegalArgumentException("No screening available."));
     }
 
     private String createTheatre(String line) {
@@ -82,9 +96,4 @@ public class MovieTheatreService {
         String[] data = line.substring(index + 1).split(";");
         return new Movie(data[0], LocalTime.parse(data[1]));
     }
-
-
-
-
-
 }
